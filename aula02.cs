8c317eec08fd9e1ebe986f58040ed42f4a50d1dc
CIEE Laboratorio <ciee.lab@serlab.intra>
2021-08-14 11:25:30 -0300

    calculo de média dos alunos// aula02

---
+++ aula02.cs
@@ -0,0 +1,12 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{ double nota1 = 2.5;
+		double nota2 = 3;
+		  double nota3 = 1.5;
+	  double media = (nota1+ nota2+ nota3)/3;
+			Console.WriteLine(media);
+	}
+}
